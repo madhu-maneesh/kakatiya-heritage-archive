5d875855a0a60cc84b202cad77dbded654d43fc4
madhu-maneesh <madhumaneeshshetty1@gmail.com>
2026-06-06 20:31:52 +0530

    Fix router initialization to include queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider } from '@tanstack/react-router'
+import { getRouter } from './router'
 
-import { routeTree } from './routeTree.gen'
 import './styles.css'
 
-const router = createRouter({ routeTree })
+const router = getRouter()
 
 declare module '@tanstack/react-router' {
   interface Register {
