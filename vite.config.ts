4a50071e19b0d4cb761ba0c4a427950d3bb2c747
madhu-maneesh <madhumaneeshshetty1@gmail.com>
2026-06-06 21:16:56 +0530

    added changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
     tsconfigPaths(),
+    tailwindcss(),
   ],
   server: {
     host: "::",
